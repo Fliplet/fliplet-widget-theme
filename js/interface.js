3eafb7e2883eb532d49cc0047b95cd9172da02ae
Hugo Carneiro <hcarneiro@weboo.biz>
2017-02-15 18:43:37 +0000

    More tweaks

--- js/interface.js
+++ js/interface.js
@@ -1,205 +1,240 @@
+window.ENV.appId = 11;
+
 Handlebars.registerHelper('setValue', function(node) {
-  var values = node.data._parent._parent.root.instance.settings.values || {};
-  return values[this.name] || this.default;
+    var values = node.data._parent._parent.root.instance.settings.values || {};
+    return values[this.name] || this.default;
 });
 
 Handlebars.registerHelper('if_eq', function(a, b, opts) {
-    if(a == b)
+    if (a == b)
         return opts.fn(this);
     else
         return opts.inverse(this);
 });
 
-Fliplet.Widget.register('com.fliplet.theme', function () {
-  var saveRequests = [];
-  var $main = $('main');
+// Flag to be used to determine if it is an initial load
+// This is to prevent theme instances from being created on first load
+var initialLoad = true;
 
-  if (!Fliplet.Env.get('appId')) {
-    throw new Error('appId is required');
-  }
+Fliplet.Widget.register('com.fliplet.theme', function() {
+    var saveRequests = [];
+    var $main = $('main');
 
-  $themeInstances = $('[data-theme-instances]');
-  $instances = $('[data-instances]');
-  $instanceEmpty = $('.instance-empty');
-  var emptyState;
-
-  function tpl(name) {
-    return Fliplet.Widget.Templates['templates.' + name];
-  }
-
-  function reloadPage() {
-    Fliplet.Studio.emit('reload-page-preview');
-  }
-
-  function init() {
-    return Fliplet.Themes.get().then(function (themes) {
-      $instances.html('');
-      emptyState = true;
-
-      themes.forEach(function (theme) {
-        if (theme.instances.length) {
-          emptyState = false;
-        }
-        theme.instances.forEach(function (instance) {
-          $instances.append(tpl('instance')({
-            instance: instance,
-            theme: theme
-          }));
-        });
-      });
-
-      // Adds/Removes empty state
-      if (emptyState) {
-        $instanceEmpty.removeClass('hidden');
-      } else {
-        $instanceEmpty.addClass('hidden');
-      }
-
-      // bind plugins on inputs
-      $instances.find('[data-type="color"]').each(function () {
-        var picker = new CP(this);
-
-        $(this).on('keyup change paste blur', function() {
-          picker.set(this.value.toLowerCase());
-          picker.trigger("change", [this.value.substring(1)], 'main-change');
-        });
-
-        picker.on("change", function(color) {
-          this.target.value = '#' + color;
-          $($(this.target).siblings('div')[0]).css('background-color', '#'+color);
-        }, 'main-change');
-
-        var colors = ['1d3f68', '00abd2', '036b95', 'ffd21d', 'ed9119', 'e03629', '831811', '5e0f0f', '23a437', '076c31'], box;
-
-        for (var i = 0, len = colors.length; i < len; ++i) {
-          box = document.createElement('span');
-          box.className = 'color-picker-box';
-          box.title = '#' + colors[i];
-          box.style.backgroundColor = '#' + colors[i];
-          box.addEventListener("click", function(e) {
-            picker.set(this.title);
-            picker.trigger("change", [this.title.slice(1)], 'main-change');
-            e.stopPropagation();
-          }, false);
-          picker.picker.firstChild.appendChild(box);
-        }
-      });
-
-    });
-  }
-
-  Fliplet.Themes.get().then(function (themes) {
-    var themeId = '';
-    $themeInstances.find('option').text('-- Select a theme');
-    themes.forEach(function (theme) {
-      $themeInstances.append(tpl('create')(theme));
-      if (theme.instances.length) {
-        themeId = theme.id;
-      }
-    });
-
-    if (themeId !== '') {
-      $themeInstances.val(themeId);
-    } else {
-      $themeInstances.val('none');
+    if (!Fliplet.Env.get('appId')) {
+        throw new Error('appId is required');
     }
 
-    $themeInstances.trigger('change');
-    $themeInstances.prop('disabled', '');
-  });
+    $themeInstances = $('[data-theme-instances]');
+    $instances = $('[data-instances]');
+    $instanceEmpty = $('.instance-empty');
+    var emptyState;
 
-  $(document).on('change', '.hidden-select', function(){
-    var selectedValue = $(this).val();
-    var selectedText = $(this).find("option:selected").text();
-    $(this).parents('.select-proxy-display').find('.select-value-proxy').text(selectedText);
-  });
-
-  $(document).on('change', '[data-theme-instances]', function (event) {
-    console.log( $(this).val() );
-    var widgetInstanceId = $(this).val();
-    // Removes all widget instances if NONE is selected
-    if (widgetInstanceId === "none" && $('[data-instances] [data-widget-id]').length) {
-      $('[data-instances] [data-widget-id]').each(function(i, el) {
-        Fliplet.API.request({
-          method: 'DELETE',
-          url: 'v1/widget-instances/' + $(el).data('instance-id'),
-          data: {
-            destroy: true
-          }
-        });
-      });
-      init().then(reloadPage);
-      return;
+    function tpl(name) {
+        return Fliplet.Widget.Templates['templates.' + name];
     }
 
-    // Check if it is the same theme you clicked
-    if ( $('[data-instances] [data-widget-id="' + widgetInstanceId + '"]').length === 0 ) {
-      // If it isn't then
-      // First removes all instances
-      $('[data-instances] [data-widget-id]').each(function(i, el) {
-        Fliplet.API.request({
-          method: 'DELETE',
-          url: 'v1/widget-instances/' + $(el).data('instance-id'),
-          data: {
-            destroy: true
-          }
-        });
-      });
+    function reloadPage() {
+        Fliplet.Studio.emit('reload-page-preview');
+    }
 
-      // Then Adds the new one
-      Fliplet.API.request({
-        method: 'POST',
-        url: 'v1/widget-instances?appId=' + Fliplet.Env.get('appId'),
-        data: {
-          widgetId: widgetInstanceId
+    function validateHEX(color) {
+        var re = /^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+        return re.test(color);
+    }
+
+    function init() {
+        return Fliplet.Themes.get().then(function(themes) {
+            $instances.html('');
+            emptyState = true;
+
+            themes.forEach(function(theme) {
+                if (theme.instances.length) {
+                    emptyState = false;
+                }
+                theme.instances.forEach(function(instance) {
+                    $instances.append(tpl('instance')({
+                        instance: instance,
+                        theme: theme
+                    }));
+                });
+            });
+
+            // Adds/Removes empty state
+            if (emptyState) {
+                $instanceEmpty.removeClass('hidden');
+            } else {
+                $instanceEmpty.addClass('hidden');
+            }
+
+            // bind plugins on inputs
+            $instances.find('[data-type="color"]').each(function() {
+                var picker = new CP(this),
+                    box_1 = document.createElement('span'),
+                    box_2 = document.createElement('span');
+
+                picker.on("enter", function() {
+                    var color = '#' + CP._HSV2HEX(this.set());
+                    box_1.title = color;
+                    box_2.title = color;
+                    box_1.style.backgroundColor = color;
+                    box_2.style.backgroundColor = color;
+                });
+
+                picker.on("change", function(color) {
+                    box_2.title = '#' + color;
+                    this.target.value = '#' + color;
+                    box_2.style.backgroundColor = '#' + color;
+                    $($(this.target).siblings('div')[0]).css('background-color', '#' + color);
+                }, '');
+
+                box_1.className = 'color-preview-1';
+                box_2.className = 'color-preview-2';
+
+                picker.picker.firstChild.appendChild(box_1);
+                picker.picker.firstChild.appendChild(box_2);
+
+                // click to reset
+                box_1.addEventListener("click", function(e) {
+                    var color = this.title;
+                    picker.set(color);
+                    box_2.title = color;
+                    box_2.style.backgroundColor = color;
+                    e.stopPropagation();
+                }, false);
+
+                // click to set
+                box_2.addEventListener("click", function(e) {
+                    var color = this.title;
+                    picker.target.value = this.title;
+                    picker.exit();
+                }, false);
+
+                // set color value on exit
+                picker.on("exit", function() {
+                    this.target.value = box_2.title;
+                });
+            });
+
+        });
+    }
+
+    Fliplet.Themes.get().then(function(themes) {
+        var themeId = '';
+        $themeInstances.find('option').text('-- Select a theme');
+        themes.forEach(function(theme) {
+            $themeInstances.append(tpl('create')(theme));
+            if (theme.instances.length) {
+                themeId = theme.id;
+            }
+        });
+
+        if (themeId !== '') {
+            $themeInstances.val(themeId);
+        } else {
+            $themeInstances.val('none');
         }
-      }).then(init).then(reloadPage);
-    }
-  });
 
-  $instances.on('submit', '[data-instance-id] form', function (event) {
-    event.preventDefault();
-
-    var $form = $(this);
-
-    var instanceId = $form.closest('[data-instance-id]').data('instance-id');
-
-    var data = $form.serializeArray().reduce(function(obj, item) {
-      obj[item.name] = item.value;
-      return obj;
-    }, {});
-
-    saveRequests.push(Fliplet.API.request({
-      url: 'v1/widget-instances/' + instanceId,
-      method: 'PUT',
-      data: {
-        package: $form.closest('[data-instance-id]').data('package-name'),
-        values: data || {}
-      }
-    }));
-  });
-
-  Fliplet.Widget.onSaveRequest(function () {
-    saveRequests = [];
-    $instances.find('[data-instance-id] form').submit();
-
-    $main.addClass('saving');
-
-    Promise.all(saveRequests).then(function () {
-      $main.removeClass('saving');
-
-      Fliplet.Widget.complete();
-      reloadPage();
-    }, function (err) {
-      $main.removeClass('saving');
-
-      var message = err.responseJSON.error && err.responseJSON.error.formatted;
-      console.warn(err.responseJSON.error);
-      alert(message);
+        // The initialLoad flag was created because this gets triggered on first load to change the label name
+        $themeInstances.trigger('change');
+        $themeInstances.prop('disabled', '');
     });
-  });
 
-  init();
+    $(document).on('change', '.hidden-select', function() {
+        var selectedValue = $(this).val();
+        var selectedText = $(this).find("option:selected").text();
+        $(this).parents('.select-proxy-display').find('.select-value-proxy').text(selectedText);
+    });
 
-  return {};
+    $(document).on('change', '[data-theme-instances]', function(event) {
+        var widgetInstanceId = $(this).val();
+        // Removes all widget instances if NONE is selected
+        if (widgetInstanceId === "none" && $('[data-instances] [data-widget-id]').length) {
+            $('[data-instances] [data-widget-id]').each(function(i, el) {
+                Fliplet.API.request({
+                    method: 'DELETE',
+                    url: 'v1/widget-instances/' + $(el).data('instance-id'),
+                    data: {
+                        destroy: true
+                    }
+                });
+            });
+            init().then(reloadPage);
+            return;
+        }
+
+        // Check if it is the same theme you selected
+        if (!initialLoad && $('[data-widget-id="' + widgetInstanceId + '"]').length === 0) {
+            // If it isn't then
+            // First removes all instances
+            $('[data-widget-id]').each(function(i, el) {
+                Fliplet.API.request({
+                    method: 'DELETE',
+                    url: 'v1/widget-instances/' + $(el).data('instance-id'),
+                    data: {
+                        destroy: true
+                    }
+                });
+            });
+
+            // Then Adds the new one
+            Fliplet.API.request({
+                method: 'POST',
+                url: 'v1/widget-instances?appId=' + Fliplet.Env.get('appId'),
+                data: {
+                    widgetId: widgetInstanceId
+                }
+            }).then(init).then(reloadPage);
+        } else {
+            init().then(reloadPage);
+            // Turns flag to false
+            // Next theme selections will create the theme instances correctly
+            initialLoad = false;
+        }
+    });
+
+    $instances.on('submit', '[data-instance-id] form', function(event) {
+        event.preventDefault();
+
+        var $form = $(this);
+
+        var instanceId = $form.closest('[data-instance-id]').data('instance-id');
+
+        var data = $form.serializeArray().reduce(function(obj, item) {
+            obj[item.name] = item.value;
+            return obj;
+        }, {});
+
+        saveRequests.push(Fliplet.API.request({
+            url: 'v1/widget-instances/' + instanceId,
+            method: 'PUT',
+            data: {
+                package: $form.closest('[data-instance-id]').data('package-name'),
+                values: data || {}
+            }
+        }));
+    });
+
+    Fliplet.Widget.onSaveRequest(function() {
+        saveRequests = [];
+        $instances.find('[data-instance-id] form').submit();
+
+        $main.addClass('saving');
+
+        Promise.all(saveRequests).then(function() {
+            $main.removeClass('saving');
+
+            Fliplet.Widget.complete();
+            reloadPage();
+        }, function(err) {
+            $main.removeClass('saving');
+
+            var message = err.responseJSON.error && err.responseJSON.error.formatted;
+            console.warn(err.responseJSON.error);
+            alert(message);
+        });
+    });
+
+    init();
+
+    return {};
 });
--- js/interface.templates.js
+++ js/interface.templates.js
@@ -11,31 +11,31 @@ this["Fliplet"]["Widget"]["Templates"]["templates.create"] = Handlebars.template
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</option>\r\n";
+    + "</option>\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.instance"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {};
 
-  return "      <div>\r\n        <h4>"
+  return "      <div>\n        <h4>"
     + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h4>\r\n        <ul>\r\n"
+    + "</h4>\n        <ul>\n"
     + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.variables : depth0),{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "        </ul>\r\n      </div>\r\n";
+    + "        </ul>\n      </div>\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing;
 
-  return "            <li>\r\n              <div class=\"form-group\">\r\n                <div class=\"col-sm-4 control-label\">\r\n                  <label>"
+  return "            <li>\n              <div class=\"form-group\">\n                <div class=\"col-sm-4 control-label\">\n                  <label>"
     + container.escapeExpression(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === "function" ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
-    + "</label>\r\n                </div>\r\n                <div class=\"col-sm-8\">\r\n"
+    + "</label>\n                </div>\n                <div class=\"col-sm-8\">\n"
     + ((stack1 = (helpers.if_eq || (depth0 && depth0.if_eq) || alias2).call(alias1,(depth0 != null ? depth0.type : depth0),"color",{"name":"if_eq","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
-    + "                </div>\r\n              </div>\r\n            </li>\r\n";
+    + "                </div>\n              </div>\n            </li>\n";
 },"3":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "                  <div class=\"input-group\">\r\n                    <div class=\"input-group-addon\" style=\"background-color:"
+  return "                  <div class=\"input-group\">\n                    <div class=\"input-group-addon\" style=\"background-color:"
     + alias4(((helper = (helper = helpers.setValue || (depth0 != null ? depth0.setValue : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"setValue","hash":{},"data":data}) : helper)))
-    + "\">&nbsp;&nbsp;&nbsp;</div>\r\n                    <input class=\"form-control\" type=\"text\" data-type=\""
+    + "\">&nbsp;&nbsp;&nbsp;</div>\n                    <input class=\"form-control\" type=\"text\" data-type=\""
     + alias4(((helper = (helper = helpers.type || (depth0 != null ? depth0.type : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data}) : helper)))
     + "\" name=\""
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
@@ -43,7 +43,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.instance"] = Handlebars.templa
     + alias4(((helper = (helper = helpers["default"] || (depth0 != null ? depth0["default"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"default","hash":{},"data":data}) : helper)))
     + "\" value=\""
     + alias4(((helper = (helper = helpers.setValue || (depth0 != null ? depth0.setValue : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"setValue","hash":{},"data":data}) : helper)))
-    + "\">\r\n                  </div>\r\n";
+    + "\">\n                  </div>\n";
 },"5":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -55,7 +55,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.instance"] = Handlebars.templa
     + alias4(((helper = (helper = helpers["default"] || (depth0 != null ? depth0["default"] : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"default","hash":{},"data":data}) : helper)))
     + "\" value=\""
     + alias4(((helper = (helper = helpers.setValue || (depth0 != null ? depth0.setValue : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"setValue","hash":{},"data":data}) : helper)))
-    + "\" />\r\n";
+    + "\" />\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
@@ -65,7 +65,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.instance"] = Handlebars.templa
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.instance : depth0)) != null ? stack1.widgetId : stack1), depth0))
     + "\" data-package-name=\""
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.theme : depth0)) != null ? stack1["package"] : stack1), depth0))
-    + "\">\r\n  <p>Use the fields below to change the styles of your app.</p>\r\n  <form class=\"form-horizontal\">\r\n"
+    + "\">\n  <p>Use the fields below to change the styles of your app.</p>\n  <form class=\"form-horizontal\">\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},((stack1 = ((stack1 = (depth0 != null ? depth0.theme : depth0)) != null ? stack1.settings : stack1)) != null ? stack1.configuration : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "  </form>\r\n</section>\r\n";
+    + "  </form>\n</section>\n";
 },"useData":true});
