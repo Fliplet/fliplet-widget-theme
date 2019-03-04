fe16f60be8cb9efe29071ae25187b335d91dc375
hcarneiro <hcarneiro@weboo.biz>
2019-03-04 18:29:57 +0000

    Way to save inheritance settings

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,9 @@ import bus from '../libs/bus'
 export const state = {
   componentOverlay: undefined,
   dataToSave: undefined,
-  debouncedSave: _.debounce(emitSavedData, 500)
+  inheritanceToSave: undefined,
+  debouncedSave: _.debounce(emitSavedData, 500),
+  debouncedSaveInheritance: _.debounce(emitSavedInheritance, 500)
 }
 
 export function openComponentSettings(overlayName = '', options) {
@@ -20,6 +22,7 @@ export function openComponentSettings(overlayName = '', options) {
   }
 
   state.componentOverlay = overlay
+  bus.$emit('component-overlay-opened')
 }
 
 export function closeComponentSettings() {
@@ -30,7 +33,16 @@ function emitSavedData() {
   bus.$emit('field-saved', state.dataToSave)
 }
 
+function emitSavedInheritance() {
+  bus.$emit('inheritance-saved', state.inheritanceToSave)
+}
+
 export function saveFieldData(data) {
   state.dataToSave = _.pick(data, ['name', 'value'])
   state.debouncedSave()
+}
+
+export function saveInheritanceData(data) {
+  state.inheritanceToSave = _.pick(data, ['name', 'value'])
+  state.debouncedSaveInheritance()
 }
