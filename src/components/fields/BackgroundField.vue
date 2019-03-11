2bab6b3269421791383596b2dfedd7379a29a037
hcarneiro <hcarneiro@weboo.biz>
2019-03-11 18:58:16 +0000

    initial markup for the Background field

---
+++ src/components/fields/BackgroundField.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="background-field-holder">
+    <div class="checkbox-holder inline-circle" v-for="(option, idx) in options" :key="idx">
+      <input type="checkbox" :id="'checkbox-' + option" :value="option" v-model="value">
+      <label :for="'checkbox-' + option">
+        <span class="check-icon"></span> {{ option }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import { state, getDefaultFieldValue } from '../../store'
+
+export default {
+  data() {
+    return {
+      state,
+      value: this.savedValue || getDefaultFieldValue(this.data.fieldConfig),
+      options: ['None', 'Image', 'Color']
+    }
+  },
+  props: {
+    data: Object,
+    savedValue: String
+  },
+  watch: {
+    value(newVal, oldVal) {
+      console.log('New value', newVal)
+      console.log('Old value', oldVal)
+    }
+  },
+  mounted() {
+    console.log('Default value', this.value)
+  }
+}
+</script>
