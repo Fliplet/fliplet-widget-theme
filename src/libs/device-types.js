225bd6230919d8b7d2bacd94f8e798593120a4c1
hcarneiro <hcarneiro@weboo.biz>
2019-03-11 12:13:35 +0000

    tabs and device types lib

---
+++ src/libs/device-types.js
@@ -0,0 +1,19 @@
+const tabs = [
+  {
+    name: 'Mobile',
+    type: 'mobile',
+    icon: 'fa fa-mobile'
+  },
+  {
+    name: 'Tablet',
+    type: 'tablet',
+    icon: 'fa fa-tablet'
+  },
+  {
+    name: 'Desktop',
+    type: 'desktop',
+    icon: 'fa fa-desktop'
+  }
+]
+
+export default tabs
