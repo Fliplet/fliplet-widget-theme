e927e4c9d80dc1464ee3963f499c1f83a30e4a90
hcarneiro <hcarneiro@weboo.biz>
2019-06-19 10:57:00 +0100

    Better polyfill

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import Application from './Application.vue'
 import InheritDot from './components/UI/InheritDot.vue'
 
