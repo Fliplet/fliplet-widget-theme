2b199178a71ffa4b8a7552aa1afd0429f4b9de58
Veljko Popović <veljko_popovic_90@hotmail.com>
2026-01-20 18:15:05 +0100

    theme processor

--- src/libs/theme-processor.js
+++ src/libs/theme-processor.js
@@ -1,75 +1,103 @@
 // Property mapping configuration
 const PROPERTY_MAPPINGS = {
   highlightColors: new Set([
-    'highlightColor',
-    'headingFourColor',
-    'headingSixColor',
-    'headingFiveColor',
-    'headingThreeColor',
-    'headingTwoColor',
     'headingOneColor',
-    'menuBottomBarBackgroundColor',
+    'headingTwoColor',
+    'headingThreeColor',
+    'headingFourColor',
+    'headingFiveColor',
+    'headingSixColor',
+    'textHeadingOneColor',
+    'textHeadingTwoColor',
+    'textHeadingThreeColor',
+    'textHeadingFourColor',
+    'textHeadingFiveColor',
+    'textHeadingSixColor',
+    'menuBottomBarActiveFontColor',
     'primaryButtonColor',
-    'secondaryButtonTextColor',
-    'secondaryButtonBorderColor',
-    'secondaryButtonHoverTextColor',
-    'secondaryButtonHoverBorderColor',
+    'primaryButtonHoverColor',
     'linkColor',
-    'titleColor',
-    'listSmallTitleColor',
-    'listLargeTitleColor',
-    'loginButtonColor',
-    'onboardingPaginationChevronsColor',
-    'onboardingPaginationBulletColorActive',
-    'rssTitleColor',
-    'lfdFontColor',
-    'newsFeedTitleFontColor',
-    'agendaTitleFontColor',
-    'smallHCardTitleFontColor',
-    'simpleListTitleFontColor',
-    'smallCardDetailTitleFontColor',
-    'newsFeedDetailTitleFontColor',
-    'agendaDetailTitleFontColor',
-    'smallHCardDetailTitleFontColor',
+    'linkHoverColor',
+    'appListPreparingIconColor',
+    'appListProgressBarColor',
+    'appListLoader',
+    'chatHeadingsColor',
+    'chatIconsAndLinksColor',
+    'chatButtonBackground',
+    'chatContactCardBackgroundSelected',
+    'chatOwnBubbleBackground',
+    'chartColor1',
+    'directoryActiveListColor',
+    'directoryActiveListTitleColor',
+    'directoryActiveListDescColor',
+    'formInputBorderFocusColor',
+    'formToggleActiveBackgroundColor',
+    'formSelectArrowBackground',
+    'formStarRating',
+    'formStarRatingSelected',
+    'formSliderHandle',
+    'formTypeaheadBackgroundColor',
+    'formRequiredColor',
+    'listMyListToggleColor',
+    'listSmallMyListToggleColor',
+    'listLargeMyListToggleColor',
+    'listSmallIconColor',
+    'listLargeIconColor',
+    'listSmallMyListSwipeRightColor',
+    'listLargeMyListSwipeRightColor',
+    'smallCardListDetailOverlayLineUnderTitleColor',
+    'newsFeedListDetailOverlayLineUnderTitleColor',
+    'agendaListDetailOverlayLineUnderTitleColor',
+    'smallHCardListDetailOverlayLineUnderTitleColor',
+    'smallCardFilterIconActiveColor',
+    'newsFeedFilterIconActiveColor',
+    'agendaFilterIconActiveColor',
+    'simpleListFilterIconActiveColor',
+    'smallCardSearchIconColor',
+    'newsFeedSearchIconColor',
+    'agendaSearchIconColor',
+    'simpleListSearchIconColor',
+    'smallCardSearchFieldBorderActiveColor',
+    'newsFeedSearchFieldBorderActiveColor',
+    'agendaSearchFieldBorderActiveColor',
+    'simpleListSearchFieldBorderActiveColor',
+    'smallCardBookmarkIconActiveColor',
+    'newsFeedBookmarkIconActiveColor',
+    'agendaBookmarkIconActiveColor',
+    'simpleListBookmarkIconActiveColor',
     'smallCardAddButtonBackground',
     'newsFeedAddButtonBackground',
     'agendaAddButtonBackground',
     'simpleListAddButtonBackground',
     'lfdAgendaTopTextActiveColor',
-    'markerLabelBackground',
-    'mapLabelBackground',
-    'controlsBackground',
-    'mapSearchOverlayText',
-    'formInputBorderFocusColor',
-    'formToggleActiveBackgroundColor',
-    'formSelectArrowBackground',
-    'formStarRating',
-    'newsFeedFilterIconActiveColor',
-    'newsFeedBookmarkIconActiveColor',
-    'newsFeedLikeIconActiveColor',
-    'agendaItemTimeFontColor',
-    'agendaFilterIconActiveColor',
-    'agendaBookmarkIconActiveColor',
-    'smallCardFilterIconActiveColor',
-    'smallCardBookmarkIConActiveColor'
+    'onboardingPaginationBulletColorActive',
+    'onboardingHeadingColor',
+    'paginationBulletColorActive',
+    'sliderHeadingColor',
+    'chevronSeparator',
+    'iconColor',
+    'loginButtonColor',
+    'loginButtonBorderColor',
+    'loginButtonHoverColor',
+    'loginButtonHoverBorderColor',
+    'lockInputFocus',
+    'pushAcceptButton',
+    'rssHighlight'
   ]),
   secondaryColors: new Set([
-    'secondaryColor',
+    'secondaryButtonColor',
+    'secondaryButtonHoverColor',
+    'chartColor2',
+    'formSliderFill',
     'menuBottomBarActiveFontColor',
     'listSeparatorColor',
     'listChevronColor',
-    'listSmallIconColor',
-    'listSmallChevronColor',
     'listSmallSeparatorColor',
+    'listSmallChevronColor',
     'listLargeSeparatorColor',
     'listLargeChevronColor',
-    'listLargeIconColor',
     'rssSeparatorColor',
     'rssChevronColor',
-    'smallCardListDetailOverlayLineUnderTitleColor',
-    'newsFeedListDetailOverlayLineUnderTitleColor',
-    'agendaListDetailOverlayLineUnderTitleColor',
-    'smallHCardListDetailOverlayLineUnderTitleColor',
     'smallCardListDetailOverlayIconsColor',
     'agendaListDetailOverlayIconsColor',
     'smallHCardListDetailOverlayIconsColor',
@@ -78,10 +106,6 @@ const PROPERTY_MAPPINGS = {
     'newsFeedFilterIconColor',
     'agendaFilterIconColor',
     'simpleListFilterIconColor',
-    'smallCardSearchIconColor',
-    'newsFeedSearchIconColor',
-    'agendaSearchIconColor',
-    'simpleListSearchIconColor',
     'smallCardSearchFieldBorderColor',
     'newsFeedSearchFieldBorderColor',
     'agendaSearchFieldBorderColor',
@@ -97,44 +121,283 @@ const PROPERTY_MAPPINGS = {
     'simpleListLikeIconColor',
     'formTypeaheadBackgroundColor'
   ]),
-  paragraphTextColors: new Set([
-    'quickTextColor',
-    'newsFeedDetailTextFontColor',
+  quickTextColors: new Set([
+    'paragraphTextColor',
+    'menuTopNavFontColor',
+    'menuBackButtonColor',
+    'menuButtonColor',
+    'menuFontColor',
+    'menuCloseButtonColor',
+    'menuFooterFontColor',
+    'menuPushTopNavFontColor',
+    'menuPushBackButtonColor',
+    'menuPushButtonColor',
+    'menuPushFontColor',
+    'menuPushCloseButtonColor',
+    'menuPushFooterFontColor',
+    'menuSlideTopNavFontColor',
+    'menuSlideBackButtonColor',
+    'menuSlideButtonColor',
+    'menuSlideFontColor',
+    'menuSlideCloseButtonColor',
+    'menuSlideFooterFontColor',
+    'menuSwipeTopNavFontColor',
+    'menuSwipeBackButtonColor',
+    'menuSwipeButtonColor',
+    'menuSwipeFontColor',
+    'menuSwipeCloseButtonColor',
+    'menuSwipeFooterFontColor',
+    'menuExpFontColor',
+    'menuExpButtonColor',
+    'menuExpButtonIconColorOpen',
+    'menuExpBackgroundOpen',
+    'menuExpFooterFontColor',
+    'menuBottomBarBackgroundColor',
+    'menuBottomBarFontColor',
+    'accordionHeadingText',
+    'accordionHeadingTextActive',
+    'accordionHeadingChevron',
+    'accordionHeadingChevronActive',
+    'appListHeadingsColor',
+    'appListDownloadIconColor',
+    'appListInitialIconColor',
+    'appListTextColor',
+    'appListLoginOverlayTextColor',
+    'appListLoginOverlayNavBarTextColor',
+    'appListCloseButtonTextColor',
+    'chatTextColor',
+    'chatButtonTextColor',
+    'chatContactCardText',
+    'chatContactCardTextSelected',
+    'chatSeparatorTextColor',
+    'chatOthersBubbleTextColor',
+    'directoryTextColor',
+    'directoryActiveListTitleColor',
+    'directoryActiveListDescColor',
+    'directoryOverlayContentTitleColor',
+    'directoryOverlayContentValueColor',
+    'formLabelColor',
+    'formTextInputsFontColor',
+    'formSelectTextColor',
+    'formSignatureClearTextColor',
+    'gridTitleColor',
+    'gridDescriptionColor',
+    'titleColor',
     'descriptionColor',
-    'newsFeedDescriptionFontColor',
+    'listSmallTitleColor',
     'listSmallDescriptionColor',
+    'listLargeTitleColor',
     'listLargeDescriptionColor',
-    'onboardingTextColor',
-    'rssDescriptionColor',
+    'lfdFontColor',
     'smallCardDescriptionFontColor',
-    'newsFeedDescriptionFontColor',
-    'agendaDescriptionFontColor',
-    'simpleListDescriptionFontColor',
     'smallCardSecondDescriptionFontColor',
-    'newsFeedSecondDescriptionFontColor',
-    'simpleListSecondDescriptionFontColor',
+    'smallCardDetailTitleFontColor',
     'smallCardDetailDescriptionFontColor',
-    'newsFeedDetailDescriptionFontColor',
-    'agendaDetailDescriptionFontColor',
-    'smallHCardDetailDescriptionFontColor',
     'smallCardDetailSecondDescriptionFontColor',
-    'agendaDetailSecondDescriptionFontColor',
-    'smallHCardDetailSecondDescriptionFontColor',
     'lfdDetailFontColor',
-    'newsFeedDetailSecondDescriptionFontColor',
-    'agendaDetailTextFontColor',
-    'smallHCardDetailTextFontColor',
-    'simpleListDetailTextFontColor',
+    'smallCardDetailLabelsFontColor',
     'lfdOverlayFontColor',
+    'newsFeedTitleFontColor',
+    'newsFeedDescriptionFontColor',
+    'newsFeedSecondDescriptionFontColor',
+    'newsFeedDetailTitleFontColor',
+    'newsFeedDetailDescriptionFontColor',
+    'newsFeedDetailSecondDescriptionFontColor',
+    'newsFeedDetailTextFontColor',
     'newsFeedOverlayFontColor',
+    'newsFeedSearchFieldTextColor',
+    'agendaTitleFontColor',
+    'agendaDescriptionFontColor',
+    'agendaItemTimeFontColor',
+    'agendaDetailTitleFontColor',
+    'agendaDetailDescriptionFontColor',
+    'agendaDetailSecondDescriptionFontColor',
+    'agendaDetailTextFontColor',
     'agendaOverlayFontColor',
+    'agendaSearchFieldTextColor',
+    'smallHCardTitleFontColor',
+    'smallHCardDetailTitleFontColor',
+    'smallHCardDetailDescriptionFontColor',
+    'smallHCardDetailSecondDescriptionFontColor',
+    'smallHCardDetailTextFontColor',
+    'smallHCardDetailLabelsFontColor',
+    'simpleListTitleFontColor',
+    'simpleListDescriptionFontColor',
+    'simpleListSecondDescriptionFontColor',
+    'simpleListDetailTextFontColor',
+    'simpleListDetailLabelsFontColor',
     'simpleListOverlayFontColor',
-    'lfdCommentsFontColor',
+    'simpleListSearchFieldTextColor',
     'simpleListCommentsFontColor',
     'lfdAgendaTopTextColor',
     'lfdAgendaDatesFontColor',
-    'formLabelColor',
-    'formRequiredColor'
+    'lfdCommentsFontColor',
+    'lfdChevronColor',
+    'lockChevronColor',
+    'chevronColor',
+    'loginButtonTextColor',
+    'loginButtonHoverTextColor',
+    'onboardingSkipTextColor',
+    'onboardingPaginationChevronsColor',
+    'onboardingTextColor',
+    'panelTitleColor',
+    'panelDescriptionColor',
+    'imageOverlayTextColor',
+    'imageOverlayNavTextColor',
+    'pushTextColor',
+    'rssTitleColor',
+    'rssDescriptionColor',
+    'paginationChevronsColor',
+    'sliderTextColor'
+  ]),
+  quickBackgrounds: new Set([
+    'Color',
+    'menuTopNavBackground',
+    'menuBackgroundColor',
+    'menuPushTopNavBackground',
+    'menuPushBackgroundColor',
+    'menuSlideTopNavBackground',
+    'menuSlideBackgroundColor',
+    'menuSwipeTopNavBackground',
+    'menuSwipeBackgroundColor',
+    'menuExpFontColor',
+    'menuExpButtonIconColor',
+    'menuExpButtonColorOpen',
+    'menuBottomActiveBackground',
+    'primaryButtonTextColor',
+    'primaryButtonHoverTextColor',
+    'secondaryButtonTextColor',
+    'secondaryButtonHoverTextColor',
+    'accordionContentBackgroundColor',
+    'appListLoginOverlayColor',
+    'appListLoginOverlayNavBar',
+    'appListCloseButtonColor',
+    'chatBackgroundColor',
+    'chatContactCardBackground',
+    'directoryBackgroundColor',
+    'directoryOverlayNavColor',
+    'formToggleColor',
+    'formSelectArrow',
+    'formSliderValue',
+    'formTypeaheadTextColor',
+    'gridTextBackgroundColor',
+    'listBackgroundColor',
+    'listSmallBackgroundColor',
+    'listLargeBackgroundColor',
+    'listSmallIconBackgroundColor',
+    'listLargeIconBackgroundColor',
+    'lfdBackgroundColor',
+    'lfdDetailBackgroundColor',
+    'smallCardSearchFieldBackgroundColor',
+    'newsFeedSearchFieldBackgroundColor',
+    'agendaSearchFieldBackgroundColor',
+    'simpleListSearchFieldBackgroundColor',
+    'smallCardAddButtonIcon',
+    'newsFeedAddButtonIcon',
+    'agendaAddButtonIcon',
+    'simpleListAddButtonIcon',
+    'lfdOverlayBackgroundColor',
+    'newsFeedListItemBackground',
+    'newsFeedListDetailOverlayBackground',
+    'lfdCommentsBackgroundColor',
+    'simpleListCommentsBackgroundColor',
+    'lfdAgendaTopBackgroundColor',
+    'lfdAgendaDatesBackgroundColor',
+    'smallHCardListItemBackground',
+    'smallHCardListDetailOverlayBackground',
+    'simpleListItemBackground',
+    'simpleListDetailOverlayBackground',
+    'notificationPublishedBackgroundColor',
+    'onboardingSlideBackgroundColor',
+    'onboardingPaginationChevronsBgColor',
+    'panelBackgroundColor',
+    'imageOverlayBackgroundColor',
+    'imageOverlayNavBackgroundColor',
+    'pushBackgroundColor',
+    'rssBackgroundColor'
+  ]),
+  quickHeadingFontFamilies: new Set([
+    'headingOneFontFamily',
+    'headingTwoFontFamily',
+    'headingThreeFontFamily',
+    'headingFourFontFamily',
+    'headingFiveFontFamily',
+    'headingSixFontFamily',
+    'textHeadingOneFontFamily',
+    'textHeadingTwoFontFamily',
+    'textHeadingThreeFontFamily',
+    'textHeadingFourFontFamily',
+    'textHeadingFiveFontFamily',
+    'textHeadingSixFontFamily',
+    'appListHeadingsFontFamily',
+    'onboardingHeadingFont',
+    'sliderHeadingFont'
+  ]),
+  quickTextFontFamilies: new Set([
+    'paragraphFontFamily',
+    'menuFont',
+    'menuPushFont',
+    'menuSlideFont',
+    'menuSwipeFont',
+    'menuExpFont',
+    'menuBottomFont',
+    'primaryButtonFontFamily',
+    'secondaryButtonFontFamily',
+    'linkFontFamily',
+    'linkFontFamilyHover',
+    'formLabelFontFam',
+    'formTextInputsFontFamily',
+    'formErrorTextFontFamily',
+    'gridTitleFont',
+    'gridDescriptionFont',
+    'titleFont',
+    'descriptionFont',
+    'listSmallTitleFont',
+    'listSmallDescriptionFont',
+    'listLargeTitleFont',
+    'listLargeDescriptionFont',
+    'smallCardTitleFontFamily',
+    'smallCardDescriptionFontFamily',
+    'smallCardSecondDescriptionFontFamily',
+    'smallCardDetailTitleFontFamily',
+    'smallCardDetailDescriptionFontFamily',
+    'smallCardDetailSecondDescriptionFontFamily',
+    'smallCardDetailTextFontFamily',
+    'smallCardDetailLabelsFontFamily',
+    'newsFeedTitleFontFamily',
+    'newsFeedDescriptionFontFamily',
+    'newsFeedSecondDescriptionFontFamily',
+    'newsFeedDetailTitleFontFamily',
+    'newsFeedDetailDescriptionFontFamily',
+    'newsFeedDetailSecondDescriptionFontFamily',
+    'newsFeedDetailTextFontFamily',
+    'agendaTitleFontFamily',
+    'agendaDescriptionFontFamily',
+    'agendaItemTimeFontFamily',
+    'agendaDetailTitleFontFamily',
+    'agendaDetailDescriptionFontFamily',
+    'agendaDetailSecondDescriptionFontFamily',
+    'agendaDetailTextFontFamily',
+    'smallHCardTitleFontFamily',
+    'smallHCardDetailTitleFontFamily',
+    'smallHCardDetailDescriptionFontFamily',
+    'smallHCardDetailSecondDescriptionFontFamily',
+    'smallHCardDetailTextFontFamily',
+    'smallHCardDetailLabelsFontFamily',
+    'simpleListTitleFontFamily',
+    'simpleListDescriptionFontFamily',
+    'simpleListSecondDescriptionFontFamily',
+    'simpleListDetailTextFontFamily',
+    'simpleListDetailLabelsFontFamily',
+    'directoryOverlayContentLabelFont',
+    'directoryOverlayContentValueFont',
+    'loginButtonTextFontFamily',
+    'onboardingTextFont',
+    'panelTitleFont',
+    'panelDescriptionFont',
+    'sliderTextFont',
+    'rssTitleFont',
+    'rssDescriptionFont'
   ])
 };
 
@@ -142,7 +405,10 @@ const PROPERTY_MAPPINGS = {
 const DEFAULT_FIELD_MAPPINGS = {
   highlightColor: '$highlightColor',
   secondaryColor: '$secondaryColor',
-  quickTextColor: '$quickTextColor'
+  quickTextColor: '$quickTextColor',
+  quickBackground: '$quickBackground',
+  quickHeadingFontFamily: '$quickHeadingFontFamily',
+  quickTextFontFamily: '$quickTextFontFamily'
 };
 
 /**
@@ -165,9 +431,16 @@ export function processThemeConfiguration(theme) {
       return DEFAULT_FIELD_MAPPINGS.highlightColor;
     } else if (PROPERTY_MAPPINGS.secondaryColors.has(fieldName)) {
       return DEFAULT_FIELD_MAPPINGS.secondaryColor;
-    } else if (PROPERTY_MAPPINGS.paragraphTextColors.has(fieldName)) {
+    } else if (PROPERTY_MAPPINGS.quickTextColors.has(fieldName)) {
       return DEFAULT_FIELD_MAPPINGS.quickTextColor;
+    } else if (PROPERTY_MAPPINGS.quickBackgrounds.has(fieldName)) {
+      return DEFAULT_FIELD_MAPPINGS.quickBackground;
+    } else if (PROPERTY_MAPPINGS.quickHeadingFontFamilies.has(fieldName)) {
+      return DEFAULT_FIELD_MAPPINGS.quickHeadingFontFamily;
+    } else if (PROPERTY_MAPPINGS.quickTextFontFamilies.has(fieldName)) {
+      return DEFAULT_FIELD_MAPPINGS.quickTextFontFamily;
     }
+
     return null;
   };
 
@@ -183,29 +456,35 @@ export function processThemeConfiguration(theme) {
 
     if (!theme?.settings?.configuration) {
       defaultValueCache.set(defaultFieldName, null);
+
       return null;
     }
 
     let result = null;
 
     // Use early return pattern for better performance
-    outerLoop: for (const config of theme.settings.configuration) {
+    for (const config of theme.settings.configuration) {
       if (!config.variables) continue;
-      
+
       for (const variable of config.variables) {
         if (!variable.fields) continue;
-        
+
         for (const field of variable.fields) {
           if (field.name === defaultFieldName && !isEmptyValue(field.default)) {
             result = field.default;
-            break outerLoop;
+            break;
           }
         }
+
+        if (result) break;
       }
+
+      if (result) break;
     }
 
     // Cache the result
     defaultValueCache.set(defaultFieldName, result);
+
     return result;
   };
 
@@ -213,18 +492,18 @@ export function processThemeConfiguration(theme) {
   if (theme?.settings?.configuration) {
     for (const config of theme.settings.configuration) {
       if (!config.variables) continue;
-      
+
       for (const variable of config.variables) {
         if (!variable.fields) continue;
-        
+
         for (const field of variable.fields) {
           // Check if field has no default value
           if (isEmptyValue(field.default)) {
             const defaultFieldName = getDefaultValueForField(field.name);
-            
+
             if (defaultFieldName) {
               const actualDefaultValue = getActualDefaultValue(theme, defaultFieldName);
-              
+
               if (actualDefaultValue) {
                 field.default = actualDefaultValue;
               }
@@ -239,4 +518,4 @@ export function processThemeConfiguration(theme) {
 }
 
 // Export constants for use in other modules if needed
-export { PROPERTY_MAPPINGS, DEFAULT_FIELD_MAPPINGS }; 
+export { PROPERTY_MAPPINGS, DEFAULT_FIELD_MAPPINGS };
